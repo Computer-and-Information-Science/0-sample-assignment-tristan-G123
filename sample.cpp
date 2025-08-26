0104ccbcadec8d9faa15c5f994436074daedc962
tristan-G123 <tristangannon06@gmail.com>
2025-08-26 17:38:44 -0400

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,16 @@
-// YOUR NAME HERE
+// Tristan Gannon
 #include <iostream>
 using namespace std;
 
 int main () {
-
+  //variables for first and second integers
+  int first, second;
+  //Displays text then returns user input
+  cout << "Enter first integer: ";
+  cin >> first;
+  cout << "Enter second integer: ";
+  cin >> second;
+  //adds both first and second together
+  sum = first + second;
+  cout << "/nThe sum is " << sum << "." << endl;
 }
